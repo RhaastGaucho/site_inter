2505d50c8c06de8f9ea73840252afc259c04fd7d
Mauricio Rockembach dos Santos <mauriciors289@gmail.com>
2025-08-31 14:17:15 -0300

    Implementacao do layout

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,16 +1,12 @@
-import Header from '../../components/Header/Header';
 import Layout from '../../components/Layout/Layout';
 import './About.css';
 
 function About() {
   return (
-    <div className="about-page">
-      <Header />
-      <main>
-        <h1>Página Secundária</h1>
-        <p>Esta é a página secundária.</p>
-      </main>
-    </div>
+    <Layout>
+      <h1>Página Secundária</h1>
+      <p>Esta é a página secundária.</p>
+    </Layout>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,12 @@
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
 import Layout from '../../components/Layout/Layout';
 import './Home.css';
 
 function Home() {
   return (
-    <div className="home-page">
-      <Header />
-      <main>
-        <h1>Página Principal</h1>
-        <p>Bem-vindo à página principal!</p>
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <h1>Página Principal</h1>
+      <p>Bem-vindo à página principal!</p>
+    </Layout>
   );
 }
 
